fac0241af84b39c1d6d737e89e65c184f14d354f
Yamato <kuma.y.0506@gmail.com>
2023-08-08 23:18:43 +0900

    sec5

--- swiper.js
+++ swiper.js
@@ -1,30 +1,4 @@
-// let mySwiper = new Swiper(".swiper", {
 
-//   // オプション設定
-//   loop: true,
-//   effect: 'cube',
-
-//   // 以下のコードは省略可能
-//   //ページネーション表示設定
-//   pagination: {
-//     el: ".swiper-pagination", //ページネーション要素
-//     type: "bullets", //ページネーションの種類
-//     clickable: true, //クリックに反応させる
-//   },
-
-//   //ナビゲーションボタン（矢印）表示の設定
-//   navigation: true,
-//   navigation: {
-//     nextEl: ".swiper-button-next", //「次へボタン」要素の指定
-//     prevEl: ".swiper-button-prev", //「前へボタン」要素の指定
-//   },
-
-//   // スクロールバー表示の設定
-//   scrollbar: {
-//     el: ".swiper-scrollbar", //要素の指定
-//   },
-//   //ここまで省略可能
-// });
 
 
 //2
@@ -56,4 +30,19 @@ document.addEventListener('DOMContentLoaded', function () {
     var slideText = activeSlide.querySelector('.slide-text').textContent;
     document.getElementById('text-display').textContent = slideText;
   }
+});
+
+var buttons = document.querySelectorAll('.content-button');
+var contents = document.querySelectorAll('.content');
+
+document.getElementById('content1').classList.add('active'); // デフォルト表示
+
+buttons.forEach(function(button, index) {
+  button.addEventListener('click', function() {
+    contents.forEach(function(content) {
+      content.classList.remove('active');
+    });
+
+    contents[index].classList.add('active');
+  });
 });
